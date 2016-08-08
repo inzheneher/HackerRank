400b290324f8f6b1c30216ffc797bf8f3a9304a3
inzheneher <inzheneher@ya.ru>
2016-08-08 23:10:28 +0700

    day 7

---
+++ Day_7/src/main/java/Solution.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        List<Integer> arr = new ArrayList<>();
+        for(int i=0; i < n; i++){
+            arr.add(in.nextInt());
+        }
+        in.close();
+        Collections.reverse(arr);
+        for (int i : arr){
+            System.out.print(i + " ");
+        }
+    }
+}
