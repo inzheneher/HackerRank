94f6304438c33d2fb6bdfc5cf502b3ac1875f973
inzheneher <inzheneher@ya.ru>
2016-08-24 08:27:37 +0700

    Day_21

---
+++ Day_21/src/main/java/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    static <E> void printArray(E[] a) {
+        for (E element : a) {
+            System.out.println(element + " ");
+        }
+    }
+
+
+    public static void main(String args[]) {
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+
+        printArray(intArray);
+        printArray(stringArray);
+
+        if (Solution.class.getDeclaredMethods().length > 2) {
+            System.out.println("You should only have 1 method named printArray.");
+        }
+    }
+}
