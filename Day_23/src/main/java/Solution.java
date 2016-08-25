a98715c5918d12254eba720007b7cb355fd77c33
inzheneher <inzheneher@ya.ru>
2016-08-25 23:38:48 +0700

    Day_23

---
+++ Day_23/src/main/java/Solution.java
@@ -0,0 +1,63 @@
+import java.util.LinkedList;
+import java.util.Scanner;
+
+class Node {
+    Node left, right;
+    int data;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+class Solution {
+
+
+    static void levelOrder(Node root) {
+        //Write your code here
+
+        LinkedList<Node> q = new LinkedList();
+        if (root != null) {
+            q.add(root);
+        }
+        while (!q.isEmpty()) {
+            Node t = q.removeFirst();
+            System.out.print(t.data + " ");
+            if (t.left != null) {
+                q.add(t.left);
+            }
+            if (t.right != null) {
+                q.add(t.right);
+            }
+        }
+
+    }
+
+    public static Node insert(Node root, int data) {
+        if (root == null) {
+            return new Node(data);
+        } else {
+            Node cur;
+            if (data <= root.data) {
+                cur = insert(root.left, data);
+                root.left = cur;
+            } else {
+                cur = insert(root.right, data);
+                root.right = cur;
+            }
+            return root;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        Node root = null;
+        while (T-- > 0) {
+            int data = sc.nextInt();
+            root = insert(root, data);
+        }
+        levelOrder(root);
+    }
+}
