6cfe161512708893adde1678c7748eedf430db42
inzheneher <inzheneher@ya.ru>
2016-09-22 21:39:36 +0700

    Java Regex

---
+++ JavaRegex/src/main/java/Solution.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String []args)
+    {
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext())
+        {
+            String IP = in.next();
+            System.out.println(IP.matches(new myRegex().pattern)); //new myRegex().pattern
+        }
+
+    }
+}
+
+class myRegex{
+
+    static final String pattern =
+            "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+                    "([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+}
