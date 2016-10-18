a20c0d24ea8acb116408fee9c4149517fa9454fd
inzheneher <inzheneher@ya.ru>
2016-10-18 22:09:04 +0700

    Java 1D Array

--- Day_7/src/main/java/Solution.java
+++ Day_7/src/main/java/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        List<Integer> arr = new ArrayList<>();
+        List<Integer> arr = new ArrayList<Integer>();
         for(int i=0; i < n; i++){
             arr.add(in.nextInt());
         }
