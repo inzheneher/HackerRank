dd1b158bfb5c826a19cfccdaaa2bdaf19baa8b77
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2020-03-30 13:37:22 -0400

    Have added picking numbers module

--- Picking Numbers/src/main/java/Result.java
+++ Picking Numbers/src/main/java/Result.java
@@ -1,3 +1,9 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 class Result {
 
     /*
@@ -8,8 +14,30 @@ class Result {
      */
 
     public static int pickingNumbers(List<Integer> a) {
-    // Write your code here
-        return 1;
+        // Write your code here
+        List<Integer> sortedList = a.stream().sorted().collect(toList());
+
+        List<Integer> tempList = new ArrayList<>();
+        List<List<Integer>> subArrayList = new ArrayList<>();
+
+        for (int i = 0; i < sortedList.size(); i++) {
+            if (tempList.size() > 0) {
+                if (sortedList.get(i) - tempList.get(0) > 1) {
+                    subArrayList.add(tempList);
+                    tempList = new ArrayList<>();
+                }
+                if (i + 1 == sortedList.size()) {
+                    tempList.add(sortedList.get(i));
+                    subArrayList.add(tempList);
+                    break;
+                }
+            }
+            tempList.add(sortedList.get(i));
+        }
+
+        subArrayList.sort(Comparator.comparingInt(List::size));
+
+        return subArrayList.get(subArrayList.size() - 1).size();
     }
 
 }
---
+++ Picking Numbers/src/main/java/Solution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s{2,}", "").trim().split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
