d232063c84c1a2cd4f8352931cec4cf1f3f6d133
Aleksandr Melnikov <amelnikov@eastbanctech.com>
2020-03-30 13:36:15 -0400

    Have added picking numbers module

---
+++ Picking Numbers/src/main/java/Result.java
@@ -0,0 +1,15 @@
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+    // Write your code here
+        return 1;
+    }
+
+}
